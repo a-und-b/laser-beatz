2658b383154f8ad6479fc9fc7a0f19e383ba3362
thomasschwarzott <thms.schwarzott@gmail.com>
2023-05-26 12:50:22 +0200

    fix images

--- client/globals/PageWrapper.js
+++ client/globals/PageWrapper.js
@@ -17,7 +17,7 @@ const PageWrapper = ({ children }) => {
     return (
         <Box sx={{ minHeight: '100vh', minWidth: '100vw' }}>
             <Container sx={{ minHeight: '100vh', maxWidth: 696 }}>
-                <Box sx={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', background: `url('/img/app-background.png')`, backgroundSize: 'cover', backgroundPosition: 'center', zIndex: -1 }} />
+                <Box sx={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', background: `url('/img/app-background.svg')`, backgroundSize: 'cover', backgroundPosition: 'center', zIndex: -1 }} />
                 <Box sx={{ position: 'fixed', top: 0, left: 0, width: '100%' }}>
                     <Box sx={{ width: '100%', height: '100%', position: 'absolute', background: `linear-gradient(${theme.palette.secondary.main} 50%, transparent)` }} />
                     <Box sx={{ width: '100%', height: '100%', position: 'absolute', backdropFilter: 'blur(10px)', mask: 'linear-gradient(black, transparent)' }} />
--- client/pages/testingYeet/finishedMainQuest.js
+++ client/pages/testingYeet/finishedMainQuest.js
@@ -7,7 +7,7 @@ const FinishedMainQuest = () => {
     return (
         <Grid sx={{ width: '100%', minHeight: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <Box sx={{ maxWidth: 200, mb: 2 }}>
-            <NextImage src='/img/aurora.jpg' width='250' height='250' />
+            <NextImage src={`/img/aurora-${theme.laserbeatzMode}.svg`} width='250' height='250' />
             </Box>
             <Typography variant='h5' sx={{ textAlign: 'center', mb: 2 }}>Quest abgeschlossen</Typography>
             <Typography variant='h1' sx={{ textAlign: 'center', mb: 2, color: theme.palette.primary.main }}>“Du hast dir einen Kristall verdient!“</Typography>
